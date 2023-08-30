0561ee86e41ff4551463a118d31920eb39855231
bene <matheusbenediht@gmail.com>
2023-08-29 23:28:46 -0300

    Index users

---
+++ src/main/java/com/gymapp/controllers/user/IndexUsersController.java
@@ -0,0 +1,25 @@
+package com.gymapp.controllers.user;
+
+import com.gymapp.models.entities.UserEntity;
+import com.gymapp.services.IndexUsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/index")
+public class IndexUsersController {
+
+    @Autowired
+    private IndexUsersService service;
+
+    @GetMapping(value = "/users")
+    public ResponseEntity<List<UserEntity>> index(){
+        List<UserEntity> users = service.index();
+        return ResponseEntity.ok().body(users);
+    }
+}
---
+++ src/main/java/com/gymapp/services/IndexUsersService.java
@@ -0,0 +1,21 @@
+package com.gymapp.services;
+
+import com.gymapp.models.entities.UserEntity;
+import com.gymapp.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class IndexUsersService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<UserEntity> index(){
+        List<UserEntity> users = repository.findAll();
+        return users;
+    }
+
+}
