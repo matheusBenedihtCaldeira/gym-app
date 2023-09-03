c968c267a1f5541641412b8188efa5f8d3e71ec8
bene <matheusbenediht@gmail.com>
2023-09-02 22:13:16 -0300

    Update exercise

---
+++ src/main/java/com/gymapp/controllers/exercise/UpdateExerciseController.java
@@ -0,0 +1,23 @@
+package com.gymapp.controllers.exercise;
+
+import com.gymapp.models.dto.ExerciseDTO;
+import com.gymapp.models.entities.ExerciseEntity;
+import com.gymapp.services.exercise.UpdateExerciseService;
+import com.gymapp.services.muscleGroup.UpdateMuscleGroupService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/update")
+public class UpdateExerciseController {
+
+    @Autowired
+    private UpdateExerciseService service;
+
+    @PutMapping(value = "/exercise/{id}")
+    public ResponseEntity<ExerciseEntity> update(@RequestBody ExerciseDTO data, @PathVariable Long id){
+        ExerciseEntity exercise = service.update(data, id);
+        return ResponseEntity.ok().body(exercise);
+    }
+}
---
+++ src/main/java/com/gymapp/services/exercise/UpdateExerciseService.java
@@ -0,0 +1,33 @@
+package com.gymapp.services.exercise;
+
+import com.gymapp.models.dto.ExerciseDTO;
+import com.gymapp.models.entities.ExerciseEntity;
+import com.gymapp.models.entities.MuscleGroupEntity;
+import com.gymapp.repositories.ExerciseRepository;
+import com.gymapp.repositories.MuscleGroupRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UpdateExerciseService {
+
+    @Autowired
+    private ExerciseRepository repository;
+
+    @Autowired
+    private MuscleGroupRepository muscleGroupRepository;
+
+    public ExerciseEntity update(ExerciseDTO dataDTO, Long exerciseID){
+        ExerciseEntity exerciseFromDB = repository.findById(exerciseID).get();
+        ExerciseEntity exerciseToUpdate = new ExerciseEntity();
+        MuscleGroupEntity muscleGroup = muscleGroupRepository.findById(dataDTO.getMuscleGroupId()).get();
+        exerciseToUpdate.setName(dataDTO.getName());
+        exerciseToUpdate.setDescription(dataDTO.getDescription());
+        exerciseToUpdate.setMuscleGroup(muscleGroup);
+        BeanUtils.copyProperties(exerciseToUpdate, exerciseFromDB);
+        exerciseFromDB.setId(exerciseID);
+        return repository.save(exerciseFromDB);
+    }
+
+}
