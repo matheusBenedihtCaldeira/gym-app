b03c4f682ebc905b06523e2a098336922ea8a492
bene <matheusbenediht@gmail.com>
2023-09-08 16:01:40 -0300

    update: Atualizando URLs de exercise

--- src/main/java/com/gymapp/controllers/exercise/UpdateExerciseController.java
+++ src/main/java/com/gymapp/controllers/exercise/UpdateExerciseController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "/update")
+@RequestMapping(value = "/api/update")
 public class UpdateExerciseController {
 
     @Autowired
